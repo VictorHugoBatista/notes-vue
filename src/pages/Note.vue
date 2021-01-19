2b1b16ca9429f1e6efa0344c48f65026b3efec1d
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-19 14:55:22 -0300

    Add form to Note page file

--- src/pages/Note.vue
+++ src/pages/Note.vue
@@ -1,11 +1,39 @@
 <template>
   <q-page padding>
-    Create / Edit
+    <h2 class="text-h3">
+      Create Note
+    </h2>
+    <q-form class="row q-col-gutter-md">
+      <q-input rounded outlined clearable
+      label="Title"
+      v-model="form.title"
+      class="col-md-12">
+      </q-input>
+
+      <q-input rounded outlined autogrow clearable
+      label="Body"
+      v-model="form.body"
+      class="col-md-12">
+      </q-input>
+
+      <div class="q-mt-lg">
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-md" />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
+  data () {
+    return {
+      form: {
+        title: '',
+        body: ''
+      }
+    }
+  }
 }
 </script>
